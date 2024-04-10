06dbe6b45a4b9a895ffadc95176bdc7eeeb2ec0e
xiaohuohumax <1915970986@qq.com>
2024-04-10 19:42:47 +0800

    format files

--- packages/create-bookmark-script/template-react/bm.config.js
+++ packages/create-bookmark-script/template-react/bm.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   scans: [{ root: 'src' }],
-  plugins:[react()]
+  plugins: [react()]
 });
--- packages/create-bookmark-script/template-vue/bm.config.js
+++ packages/create-bookmark-script/template-vue/bm.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   scans: [{ root: 'src' }],
-  plugins:[vue()]
+  plugins: [vue()]
 });
